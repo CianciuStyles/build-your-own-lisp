6f08cde54eb4844fb415f507a9b27885e253fb63
Stefano Cianciulli <stefano.cianciulli@gmail.com>
2022-02-28 21:52:18 +0000

    Chapter 7 - Evaluation

--- parsing.c
+++ parsing.c
@@ -28,6 +28,37 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+/* Use operator string to see which operation to perform */
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+    /* If tagged as number, return it directly */
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    /* The operator is always the second child */
+    char* op = t->children[1]->contents;
+
+    /* We store the third child in x */
+    long x = eval(t->children[2]);
+
+    /* Iterate the remaining children and combining. */
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
 int main(int argc, char** argv) {
     /* Create Some Parsers */
     mpc_parser_t* Number = mpc_new("number");
@@ -37,15 +68,15 @@ int main(int argc, char** argv) {
 
     /* Define them with the following Language */
     mpca_lang(MPCA_LANG_DEFAULT,
-        "                                                                                \
-        number   : /-?[0-9]+(\.[0-9]+)?/ ;                                                          \
-        operator : '+' | \"add\" | '-' | \"sub\" | '*' | \"mul\" | '/' | \"div\" | '%' ; \
-        expr     : <number> | '(' <operator> <expr>+ ')' ;                               \
-        lispy    : /^/ <operator> <expr>+ /$/ ;                                          \
+        "                                                  \
+        number   : /-?[0-9]+/ ;                            \
+        operator : '+' | '-' | '*' | '/' ;                 \
+        expr     : <number> | '(' <operator> <expr>+ ')' ; \
+        lispy    : /^/ <operator> <expr>+ /$/ ;            \
         ",
         Number, Operator, Expr, Lispy);
 
-    puts("Lispy Version 0.0.0.0.1");
+    puts("Lispy Version 0.0.0.0.3");
     puts("Press Ctrl+C to Exit\n");
 
     /* In a never ending loop */
@@ -56,11 +87,10 @@ int main(int argc, char** argv) {
         /* Attempt to Parse the user Input */
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
-            /* On Success Print the AST */
-            mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else {
-            /* Otherwise Print the Error */
             mpc_err_print(r.error);
             mpc_err_delete(r.error);
         }
